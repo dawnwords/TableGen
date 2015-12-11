51daf515db7fc2549bbd88418a0d51cdcb523cdf
Gu Jingxiao <gujingxiao@gmail.com>
2015-12-11 14:54:22 +0800

    + Jmeter input generator

--- src/main/java/cn/edu/fudan/se/tablegen/Main.java
+++ src/main/java/cn/edu/fudan/se/tablegen/Main.java
@@ -1,38 +1,17 @@
 package cn.edu.fudan.se.tablegen;
 
-import cn.edu.fudan.se.tablegen.core.DataGen;
-import cn.edu.fudan.se.tablegen.core.Procedure;
-import cn.edu.fudan.se.tablegen.core.Table;
-import cn.edu.fudan.se.tablegen.procedure.AlreadySelected;
-import cn.edu.fudan.se.tablegen.procedure.CourseInfo;
-import cn.edu.fudan.se.tablegen.procedure.SelectCourse;
-import cn.edu.fudan.se.tablegen.table.Course;
-import cn.edu.fudan.se.tablegen.table.Selection;
-import cn.edu.fudan.se.tablegen.table.Student;
-import cn.edu.fudan.se.tablegen.table.Teacher;
+import cn.edu.fudan.se.tablegen.core.InputGen;
+import cn.edu.fudan.se.tablegen.core.Random;
 
 /**
  * Created by Dawnwords on 2015/12/10.
  */
 public class Main {
-    private static final int TEACHER_NUM = 300;
-    private static final int COURSE_NUM = 1000;
-    private static final int COURSE_CAPACITY = 500;
-    private static final int STUDENT_NUM = 7500;
-
     public static void main(String[] args) {
-        new DataGen().dbHost("10.131.252.156").dbPort(3306).dbName("course").dbUser("root").dbPass("cloudfdse")
-                .randomSeed(333222111)
-                .procedures(new Procedure[]{
-                        new SelectCourse(),
-                        new AlreadySelected(),
-                        new CourseInfo()
-                })
-                .tables(new Table[]{
-                        new Teacher().randomNum(TEACHER_NUM),
-                        new Course().teacherNum(TEACHER_NUM).capacity(COURSE_CAPACITY).randomNum(COURSE_NUM),
-                        new Student().randomNum(STUDENT_NUM),
-                        new Selection()/*.studentNum(STUDENT_NUM).courseCapacity(COURSE_CAPACITY).randomNum(COURSE_NUM)*/
-                }).gen();
+        new InputGen()
+                .outputPath("parameter")
+                .threadNum(10)
+                .iterationNum(100)
+                .gen(new Random(12123123), 1000, 7500);
     }
 }
---
+++ src/main/java/cn/edu/fudan/se/tablegen/core/InputGen.java
@@ -0,0 +1,56 @@
+package cn.edu.fudan.se.tablegen.core;
+
+import cn.edu.fudan.se.tablegen.core.Random;
+
+import java.io.*;
+
+/**
+ * Created by Dawnwords on 2015/12/11.
+ */
+public class InputGen {
+    private int threadNum, iterationNum;
+    private String outputPath;
+
+    public InputGen threadNum(int threadNum) {
+        this.threadNum = threadNum;
+        return this;
+    }
+
+    public InputGen iterationNum(int iterationNum) {
+        this.iterationNum = iterationNum;
+        return this;
+    }
+
+    public InputGen outputPath(String outputPath) {
+        this.outputPath = outputPath;
+        File outputFile = new File(outputPath);
+        if (!outputFile.exists() || !outputFile.isDirectory()) {
+            outputFile.mkdirs();
+        }
+        return this;
+    }
+
+    public void gen(Random random, int... randomRange) {
+        for (int i = 0; i < threadNum; i++) {
+            PrintWriter out = null;
+            try {
+                out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputPath + File.separator + i + ".para")));
+                for (int j = 0; j < iterationNum; j++) {
+                    StringBuilder builder = new StringBuilder();
+                    for (int parameter : randomRange) {
+                        builder.append(1 + random.nextInt(parameter));
+                        builder.append(',');
+                    }
+                    builder.setLength(builder.length() - 1);
+                    out.println(builder.toString());
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+            }
+        }
+    }
+}
